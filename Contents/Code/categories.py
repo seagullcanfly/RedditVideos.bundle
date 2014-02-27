7de073e33a4a16f11f02760164e5bedb93d70c81
seagullcanfly <siegal.jonathan@gmail.com>
2014-02-27 10:48:25 -0500

    Update categories.py
    removing old style of categories

--- Contents/Code/categories.py
+++ Contents/Code/categories.py
@@ -5,39 +5,39 @@
 
 from utilities import list_to_string,  create_list
 
-gaming = ['games',  'gaming',  'tf2',  'starcraft',
-          'leagueoflegends', 'minecraft', 'guildwars2',
-          'masseffect', 'runescape', 'blackops2',
-          'indiegaming', 'wow', 'planetside', 'dayz', 'skyrim']
-music = ['blues', 'bluegrass', 'jazz', 'music',
-         'electronicmusic', 'ska', 'classicalmusic',
-         'listentothis', 'metal', 'hiphopheads']
-technology = ['linux', 'technology']
-sports = ['sports', 'soccer', 'formula1', 'nba']
-educational = ['diy', 'howto', 'learnuselesstalents', 'ted', 'todayilearned']
-politics = ['politics', 'atheistvids']
-generic = ['videos', 'bestofstreamingvideo']
+#gaming = ['games',  'gaming',  'tf2',  'starcraft',
+#          'leagueoflegends', 'minecraft', 'guildwars2',
+#          'masseffect', 'runescape', 'blackops2',
+#          'indiegaming', 'wow', 'planetside', 'dayz', 'skyrim']
+#music = ['blues', 'bluegrass', 'jazz', 'music',
+#         'electronicmusic', 'ska', 'classicalmusic',
+#         'listentothis', 'metal', 'hiphopheads']
+#technology = ['linux', 'technology']
+#sports = ['sports', 'soccer', 'formula1', 'nba']
+#educational = ['diy', 'howto', 'learnuselesstalents', 'ted', 'todayilearned']
+#politics = ['politics', 'atheistvids']
+#generic = ['videos', 'bestofstreamingvideo']
 
 # Master Dictionary
 
-reddit_categories = {'gaming': {'c_list': gaming,
-                                'summary': list_to_string(gaming)},
-                     'generic': {'c_list': generic,
-                                 'summary': list_to_string(generic)},
-                     'music': {'c_list': music,
-                               'summary': list_to_string(music)},
-                     'technology': {'c_list': technology,
-                                    'summary': list_to_string(technology)},
-                     'sports': {'c_list': sports,
-                                'summary': list_to_string(sports)},
-                     'educational': {'c_list': educational,
-                                     'summary': list_to_string(educational)},
-                     'politics': {'c_list': politics,
-                                  'summary': list_to_string(politics)}
-                     }
+#reddit_categories = {'gaming': {'c_list': gaming,
+#                                'summary': list_to_string(gaming)},
+#                    'generic': {'c_list': generic,
+#                                'summary': list_to_string(generic)},
+#                    'music': {'c_list': music,
+#                              'summary': list_to_string(music)},
+#                    'technology': {'c_list': technology,
+#                                   'summary': list_to_string(technology)},
+#                    'sports': {'c_list': sports,
+#                               'summary': list_to_string(sports)},
+#                    'educational': {'c_list': educational,
+#                                    'summary': list_to_string(educational)},
+#                    'politics': {'c_list': politics,
+#                                 'summary': list_to_string(politics)}
+#                    }
 # Master List
 
-master_list = create_list(reddit_categories)
+#master_list = create_list(reddit_categories)
 
 # Domain list
 
